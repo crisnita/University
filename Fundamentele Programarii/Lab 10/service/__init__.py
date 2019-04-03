e8e2433aa275109c51a5453a99d1b90741b4fb01
Cristian Nita <cristiannita99@yahoo.com>
2019-04-03 23:15:19 +0300

    removed

--- Fundamentele Programarii/Lab 10/service/__init__.py
+++
@@ -1,5 +0,0 @@
-"""
-    @author: Nita Andrei-Cristian
-    @email:  cristiannita99@yahoo.com
-    @date:   11/17/2018 16:49
-"""
--- Fundamentele Programarii/Lab 10/ui/__init__.py
+++
@@ -1,5 +0,0 @@
-"""
-    @author: Nita Andrei-Cristian
-    @email:  cristiannita99@yahoo.com
-    @date:   11/9/2018 19:06
-"""
